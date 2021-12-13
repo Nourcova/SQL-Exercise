8bdbfb2c1339306e02d8d813cc7e6e5288523e45
Nourcova <ibrahamnour22@gmail.com>
2021-12-13 13:25:18 +0200

    Creating tables

--- answers.sh
+++ answers.sh
@@ -1,3 +1,5 @@
+# Basic Queries
+
 #1
 SELECT Name FROM students;
 
@@ -19,3 +21,28 @@ UPDATE students SET Points= Points+500 WHERE Name= 'Basma';
 
 #7
 UPDATE students SET Points= Points-200 WHERE Name= 'Alex';
+
+#8 Done
+
+#9 Done
+
+#Creating Table
+
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT
+);
+
+INSERT INTO graduates (ID,Name,Age,Gender,Points)
+SELECT ID,name,age,gender,points FROM students
+WHERE name='Layal';
+
+UPDATE graduates
+SET Graduation="08/09/2018"
+WHERE Name="Layal";
+
+DELETE from students WHERE name="ahmad";
